e45f43f29e6d2597f550066a783f4c0e5ee1119a
sw333zy <sw333zy@gmail.com>
2019-03-25 14:42:27 -0400

    added department

--- src/main/java/com/example/demo/Department.java
+++ src/main/java/com/example/demo/Department.java
@@ -1,12 +1,46 @@
 package com.example.demo;
 
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
 
 @Entity
 public class Department {
-    @OneToOne
-    Supervisor supervisor;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotNull
+    private String departmentName;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.REMOVE,
+            fetch = FetchType.EAGER)
+    public Set<User> users;
+
+    public Department() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
 }
