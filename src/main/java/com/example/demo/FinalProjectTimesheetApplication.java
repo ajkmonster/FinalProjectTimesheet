c7f192bf261c7b583f1617f74983ad142770d2de
ajkmonster <phimphachanh.momo.victor@gmail.com>
2019-03-25 10:33:44 -0400

    original project

---
+++ src/main/java/com/example/demo/FinalProjectTimesheetApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectTimesheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectTimesheetApplication.class, args);
+    }
+
+}
