c52919403e82dc36da5af1f346b299694d1ed9f9
khadkaraghav <khadkaraghav@gmail.com>
2019-03-27 11:52:58 -0400

    Timesheet calculation added

--- src/main/java/com/example/demo/TimeSheet.java
+++ src/main/java/com/example/demo/TimeSheet.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Scanner;
 
 @Entity
 public class TimeSheet {
@@ -50,4 +51,40 @@ public class TimeSheet {
     public void setEndTime(String endTime) {
         this.endTime = endTime;
     }
+
+    public void timeCalculate(){
+
+        Scanner keyboard = new Scanner(System.in);
+
+        int hoursWorked = keyboard.nextInt();
+
+        double payRate = 45;
+        int otHours = hoursWorked - 40;
+
+
+        //employees work more than 40hrs
+        if (hoursWorked > 40){
+            double otPay = (otHours) * (payRate * 1.5);
+            double wages = (otPay) + (40 * payRate);
+
+            //employees who work no overtime
+        } else if (hoursWorked <= 40){
+            double wkPay = (hoursWorked) * (payRate);
+        }
+
+        //calculation for sick leave
+        double sickLeave = 0;
+        double totalHours = (hoursWorked + otHours);
+
+        if (totalHours >= 100)
+        {sickLeave = (sickLeave +2.5);}
+
+        if (totalHours >= 300)
+        {sickLeave = (sickLeave +4.5);}
+
+        if (totalHours >= 500)
+        {sickLeave = (sickLeave +5);}
+
+    }
 }
+
